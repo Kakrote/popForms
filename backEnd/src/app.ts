71a59a6f27b8f7d95433503ff0860d47547629c3
Kakrote <anshul.pundir111@gmail.com>
2026-05-12 11:24:23 +0530

    added the health check route in the backend

--- backEnd/src/app.ts
+++ backEnd/src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import router from "./routes/index.js"
 
 
 const app=express();
 
 app.use(express.json());
+app.use("/api",router);
 
 app.get("/",(req:express.Request,res:express.Response)=>{
     res.send("hello world form express 5");
